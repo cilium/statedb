cilium/statedb

commit c7b7b158dad617d9f57e5363da018aaf701eef75

part: Fix missing closing of leaf watch channel
The leaf's watch channel wasn't closed on delete.
Fix this and add a test case to check that all watch channels
of deleted nodes are closed.

part/txn.go
@@ -255,7 +255,6 @@ type deleteParent[T any] struct {
 }
 
 func (txn *Txn[T]) delete(root *header[T], key []byte) (oldValue T, hadOld bool, newRoot *header[T]) {
-
 	// Reuse the same slice in the transaction to hold the parents in order to avoid
 	// allocations. Pre-allocate 32 levels to cover most of the use-cases without
 	// reallocation.
@@ -294,6 +293,11 @@ func (txn *Txn[T]) delete(root *header[T], key []byte) (oldValue T, hadOld bool,
 
 	oldValue = leaf.value
 	hadOld = true
+
+	// Mark the watch channel of the target node for closing.
+	if this.watch != nil {
+		txn.watches[this.watch] = struct{}{}
+	}
 
 	if this == root {
 		// Target is the root, clear it.

part/part_test.go
@@ -124,6 +124,7 @@ func Test_delete(t *testing.T) {
 			assert.False(t, ok)
 		}
 		tree = txn.Commit()
+
 		assert.Equal(t, 0, tree.Len())
 		for _, i := range keys {
 			_, _, ok := tree.Get(intKey(i))
@@ -136,11 +137,14 @@ func Test_delete(t *testing.T) {
 			keys[i], keys[j] = keys[j], keys[i]
 		})
 
+		watches := map[uint64]<-chan struct{}{}
+
 		txn = tree.Txn()
 		for _, i := range keys {
 			_, hadOld = txn.Insert(intKey(i), i)
 			assert.False(t, hadOld)
-			v, _, ok := txn.Get(intKey(i))
+			v, watch, ok := txn.Get(intKey(i))
+			watches[i] = watch
 			assert.True(t, ok)
 			assert.EqualValues(t, v, i)
 		}
@@ -222,6 +226,11 @@ func Test_delete(t *testing.T) {
 		assert.False(t, ok)
 
 		tree = txn.Commit()
+
+		// Check that all the watch channels closed
+		for _, watch := range watches {
+			<-watch
+		}
 
 		// Check that everything is gone after commit.
 		for _, i := range keys {

commit 5e5ac5113c4bbed371a427548aca3454843cf9ff

part: delete: don't close channel of mutated node
When deleting, we need to ensure that the target node watch channel is
closed. However, if we mutated the node in the transaction already,
we're looking at the cloned one, hence don't close the new channel too.

The regression was found in the bigger hex iteration test, but reduced to:

node4[]:(0xc00012a300)
 leaf[30]: 30 -> 0(0xc00014a410)
 node4[31]: 31 -> 1(0xc00012a360)
  leaf[30]: 3130 -> 2(0xc00014a550)
  leaf[31]: 3131 -> 3(0xc00014a5f0)

After deleting 3 and 1

node4[]:(0xc00012a420)
 leaf[30]: 30 -> 0(0xc00014a410)
 node4[31]:(0xc00012a3c0)
  leaf[30]: 3130 -> 2(0xc00014a550

The channel of node4[31] was closed already, which is wrong. It was
closed because we deleted 3, its child, and then deleted its leaf -
which didn't clone the node again, but closed the new watch channel.

Fixes: c7b7b15 (part: Fix missing closing of leaf watch channel)

Reported-by: Jussi Maki <jussi@isovalent.com>
Signed-off-by: David Bimmler <david.bimmler@isovalent.com>

part/txn.go
@@ -299,8 +299,8 @@ func (txn *Txn[T]) delete(root *header[T], key []byte) (oldValue T, hadOld bool,
 	oldValue = leaf.value
 	hadOld = true
 
-	// Mark the watch channel of the target node for closing.
-	if this.watch != nil {
+	// Mark the watch channel of the target node for closing if not mutated already.
+	if this.watch != nil && !txn.mutated.exists(this) {
 		txn.watches[this.watch] = struct{}{}
 	}
 

part/part_test.go
@@ -597,7 +597,6 @@ func Test_lowerbound_edge_cases(t *testing.T) {
 	next(false, 0)
 
 	// Node256
-	fmt.Println("node256:")
 	for i := 1; i < 50; i += 2 { // add less than 256 for some holes in node256.children
 		n := uint32(0x20000 + i)
 		_, _, tree = tree.Insert(uint32Key(n), n)
@@ -735,6 +734,29 @@ func Test_iterate(t *testing.T) {
 			}
 		}
 
+	}
+}
+
+func Test_closed_chan_regression(t *testing.T) {
+	tree := New[uint64]()
+	_, _, tree = tree.Insert(hexKey(uint64(0)), uint64(0))
+	_, _, tree = tree.Insert(hexKey(uint64(1)), uint64(1))
+	_, _, tree = tree.Insert(hexKey(uint64(2)), uint64(2))
+	_, _, tree = tree.Insert(hexKey(uint64(3)), uint64(3))
+
+	txn := tree.Txn()
+	txn.Delete(hexKey(uint64(3)))
+	txn.Delete(hexKey(uint64(1)))
+	tree = txn.Commit()
+
+	// No reachable channel should be closed
+	for _, c := range tree.root.children() {
+		select {
+		case <-c.watch:
+			t.Logf("%x %p closed already", c.prefix, &c.watch)
+			t.FailNow()
+		default:
+		}
 	}
 }
 