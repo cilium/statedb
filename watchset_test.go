cilium/statedb

commit f0c88229f440e90a671e5410ae98652aef5ee2d4

Use reflect.Select for WatchSet
Instead of a set of goroutines doing select, use reflect.Select instead.

Benchmark before:
goos: linux
goarch: arm64
pkg: github.com/cilium/statedb
BenchmarkWatchSet_4
BenchmarkWatchSet_4-6             633849              2027 ns/op             736 B/op         15 allocs/op
BenchmarkWatchSet_16
BenchmarkWatchSet_16-6            273488              4521 ns/op            1232 B/op         18 allocs/op
BenchmarkWatchSet_128
BenchmarkWatchSet_128-6            48825             24887 ns/op            5653 B/op         28 allocs/op
BenchmarkWatchSet_1024
BenchmarkWatchSet_1024-6            5077            262523 ns/op           55920 B/op        109 allocs/op

Benchmark after:
goos: linux
goarch: arm64
pkg: github.com/cilium/statedb
BenchmarkWatchSet_4
BenchmarkWatchSet_4-6            1493523               918.2 ns/op           499 B/op          7 allocs/op
BenchmarkWatchSet_16
BenchmarkWatchSet_16-6            480783              2105 ns/op            1700 B/op          8 allocs/op
BenchmarkWatchSet_128
BenchmarkWatchSet_128-6            68299             19760 ns/op           13400 B/op          8 allocs/op
BenchmarkWatchSet_1024
BenchmarkWatchSet_1024-6            6812            174220 ns/op          105176 B/op          8 allocs/op

Signed-off-by: Jussi Maki <jussi@isovalent.com>

watchset.go
@@ -5,21 +5,21 @@ package statedb
 
 import (
 	"context"
-	"maps"
+	"reflect"
 	"slices"
 	"sync"
 	"time"
 )
 
-const watchSetChunkSize = 16
-
-type channelSet = map[<-chan struct{}]struct{}
-
 // WatchSet is a set of watch channels that can be waited on.
 type WatchSet struct {
 	mu    sync.Mutex
 	chans channelSet
+
+	cases []reflect.SelectCase // for reuse in Wait()
 }
+
+type channelSet = map[<-chan struct{}]struct{}
 
 func NewWatchSet() *WatchSet {
 	return &WatchSet{
@@ -79,13 +79,11 @@ func (ws *WatchSet) Merge(other *WatchSet) {
 // more closed channels.
 // Returns the closed channels and removes them from the set.
 func (ws *WatchSet) Wait(ctx context.Context, settleTime time.Duration) ([]<-chan struct{}, error) {
-	innerCtx, cancel := context.WithCancel(ctx)
+	innerCtx, cancel := context.WithTimeout(ctx, settleTime)
 	defer cancel()
 
 	ws.mu.Lock()
 	defer ws.mu.Unlock()
-
-	closedChannels := &closedChannelsSlice{}
 
 	// No channels to watch? Just watch the context.
 	if len(ws.chans) == 0 {
@@ -93,107 +91,39 @@ func (ws *WatchSet) Wait(ctx context.Context, settleTime time.Duration) ([]<-cha
 		return nil, ctx.Err()
 	}
 
-	// Collect the channels into a slice. The slice length is rounded to a full
-	// chunk size.
-	chans := slices.Collect(maps.Keys(ws.chans))
-	chunkSize := 16
-	roundedSize := len(chans) + (chunkSize - len(chans)%chunkSize)
-	chans = slices.Grow(chans, roundedSize)[:roundedSize]
-	haveResult := make(chan struct{}, 1)
-
-	var wg sync.WaitGroup
-	chunks := slices.Chunk(chans, chunkSize)
-	for chunk := range chunks {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			watch16(haveResult, closedChannels, innerCtx.Done(), chunk)
-		}()
+	// Construct []SelectCase slice. Reuse the previous allocation.
+	ws.cases = slices.Grow(ws.cases, 1+len(ws.chans))
+	cases := ws.cases[:1+len(ws.chans)]
+	cases[0] = reflect.SelectCase{
+		Dir:  reflect.SelectRecv,
+		Chan: reflect.ValueOf(innerCtx.Done()),
 	}
-
-	// Wait for the first closed channel to be seen. If [settleTime] is set,
-	// then wait a bit longer for more.
-	select {
-	case <-haveResult:
-		if settleTime > 0 {
-			select {
-			case <-time.After(settleTime):
-			case <-ctx.Done():
-			}
+	casesIndex := 1
+	for ch := range ws.chans {
+		cases[casesIndex] = reflect.SelectCase{
+			Dir:  reflect.SelectRecv,
+			Chan: reflect.ValueOf(ch),
 		}
-	case <-ctx.Done():
+		casesIndex++
 	}
 
-	// Stop waiting for more channels to close
-	cancel()
-	wg.Wait()
+	var closedChannels []<-chan struct{}
+
+	// Collect closed channels until [innerCtx] is cancelled.
+	for {
+		chosen, _, _ := reflect.Select(cases)
+		if chosen == 0 /* == innerCtx.Done() */ {
+			break
+		}
+		closedChannels = append(closedChannels, cases[chosen].Chan.Interface().(<-chan struct{}))
+		cases[chosen] = cases[len(cases)-1]
+		cases = cases[:len(cases)-1]
+	}
 
 	// Remove the closed channels from the watch set.
-	for _, ch := range closedChannels.chans {
+	for _, ch := range closedChannels {
 		delete(ws.chans, ch)
 	}
 
-	return closedChannels.chans, ctx.Err()
-}
-
-func watch16(haveClosed chan struct{}, closedChannels *closedChannelsSlice, stop <-chan struct{}, chans []<-chan struct{}) {
-	for {
-		closedIndex := -1
-		select {
-		case <-stop:
-			return
-		case <-chans[0]:
-			closedIndex = 0
-		case <-chans[1]:
-			closedIndex = 1
-		case <-chans[2]:
-			closedIndex = 2
-		case <-chans[3]:
-			closedIndex = 3
-		case <-chans[4]:
-			closedIndex = 4
-		case <-chans[5]:
-			closedIndex = 5
-		case <-chans[6]:
-			closedIndex = 6
-		case <-chans[7]:
-			closedIndex = 7
-		case <-chans[8]:
-			closedIndex = 8
-		case <-chans[9]:
-			closedIndex = 9
-		case <-chans[10]:
-			closedIndex = 10
-		case <-chans[11]:
-			closedIndex = 11
-		case <-chans[12]:
-			closedIndex = 12
-		case <-chans[13]:
-			closedIndex = 13
-		case <-chans[14]:
-			closedIndex = 14
-		case <-chans[15]:
-			closedIndex = 15
-		}
-		closedChannels.append(chans[closedIndex])
-		chans[closedIndex] = nil
-		if haveClosed != nil {
-			select {
-			case haveClosed <- struct{}{}:
-				haveClosed = nil
-			default:
-			}
-		}
-	}
-}
-
-type closedChannelsSlice struct {
-	mu    sync.Mutex
-	chans []<-chan struct{}
-}
-
-func (ccs *closedChannelsSlice) append(ch <-chan struct{}) {
-	ccs.mu.Lock()
-	ccs.chans = append(ccs.chans, ch)
-	ccs.mu.Unlock()
+	return closedChannels, ctx.Err()
 }

commit 0539f4b19c5bc244542f0b1b8b86d7fa535c664a

watchset: Fix WatchSet.Wait() to not return empty closed channels
As the comment on Wait() implied the intended semantics for Wait() was to
wait for [settleTime] after the first channel has closed, but instead it
returned with empty closed channels when [settleTime] expired.

Fix this by first watching for 'ctx.Done()' and when a first closed channel
is encountered switch to a context with a [settleTime] timeout to gather
more channels.

Fixes: f0c88229f440 ("Use reflect.Select for WatchSet")
Signed-off-by: Jussi Maki <jussi.maki@isovalent.com>

watchset.go
@@ -77,11 +77,9 @@ func (ws *WatchSet) Merge(other *WatchSet) {
 // Wait for channels in the watch set to close or the context is cancelled.
 // After the first closed channel is seen Wait will wait [settleTime] for
 // more closed channels.
+// If [settleTime] is 0 waits until [ctx] cancelled or any channel closes.
 // Returns the closed channels and removes them from the set.
 func (ws *WatchSet) Wait(ctx context.Context, settleTime time.Duration) ([]<-chan struct{}, error) {
-	innerCtx, cancel := context.WithTimeout(ctx, settleTime)
-	defer cancel()
-
 	ws.mu.Lock()
 	defer ws.mu.Unlock()
 
@@ -94,10 +92,14 @@ func (ws *WatchSet) Wait(ctx context.Context, settleTime time.Duration) ([]<-cha
 	// Construct []SelectCase slice. Reuse the previous allocation.
 	ws.cases = slices.Grow(ws.cases, 1+len(ws.chans))
 	cases := ws.cases[:1+len(ws.chans)]
+
+	// Add [ctx.Done()] to stop when [ctx] is cancelled.
 	cases[0] = reflect.SelectCase{
 		Dir:  reflect.SelectRecv,
-		Chan: reflect.ValueOf(innerCtx.Done()),
+		Chan: reflect.ValueOf(ctx.Done()),
 	}
+
+	// Add the cases from the watch set.
 	casesIndex := 1
 	for ch := range ws.chans {
 		cases[casesIndex] = reflect.SelectCase{
@@ -109,20 +111,41 @@ func (ws *WatchSet) Wait(ctx context.Context, settleTime time.Duration) ([]<-cha
 
 	var closedChannels []<-chan struct{}
 
-	// Collect closed channels until [innerCtx] is cancelled.
-	for {
+	// At the end remove the closed channels from the watch set.
+	defer func() {
+		for _, ch := range closedChannels {
+			delete(ws.chans, ch)
+		}
+	}()
+
+	// Wait for the first channel to close and shift it out from [cases]
+	chosen, _, _ := reflect.Select(cases)
+	if chosen == 0 {
+		return nil, ctx.Err()
+	}
+	closedChannels = append(closedChannels, cases[chosen].Chan.Interface().(<-chan struct{}))
+	cases[chosen] = cases[len(cases)-1]
+	cases = cases[:len(cases)-1]
+
+	// If nothing else than context channel remains or we don't want to wait for further channels
+	// to close then we're done.
+	if len(cases) == 1 || settleTime == 0 {
+		return closedChannels, nil
+	}
+
+	// Swap out the 'ctx.Done()' to a context that times out when [settleTime] expires.
+	settleCtx, cancel := context.WithTimeout(ctx, settleTime)
+	defer cancel()
+	cases[0].Chan = reflect.ValueOf(settleCtx.Done())
+
+	for len(cases) > 1 {
 		chosen, _, _ := reflect.Select(cases)
-		if chosen == 0 /* == innerCtx.Done() */ {
+		if chosen == 0 /* settleCtx.Done() */ {
 			break
 		}
 		closedChannels = append(closedChannels, cases[chosen].Chan.Interface().(<-chan struct{}))
 		cases[chosen] = cases[len(cases)-1]
 		cases = cases[:len(cases)-1]
-	}
-
-	// Remove the closed channels from the watch set.
-	for _, ch := range closedChannels {
-		delete(ws.chans, ch)
 	}
 
 	return closedChannels, ctx.Err()

watchset_test.go
@@ -22,9 +22,9 @@ func TestWatchSet(t *testing.T) {
 	// Empty watch set, cancelled context.
 	ctx, cancel := context.WithCancel(context.Background())
 	cancel()
-	ch, err := ws.Wait(ctx, time.Second)
+	chs, err := ws.Wait(ctx, time.Second)
 	require.ErrorIs(t, err, context.Canceled)
-	require.Nil(t, ch)
+	require.Empty(t, chs)
 
 	// Few channels, cancelled context.
 	ch1 := make(chan struct{})
@@ -33,9 +33,35 @@ func TestWatchSet(t *testing.T) {
 	ws.Add(ch1, ch2, ch3)
 	ctx, cancel = context.WithCancel(context.Background())
 	cancel()
-	ch, err = ws.Wait(ctx, time.Second)
+	chs, err = ws.Wait(ctx, time.Second)
 	require.ErrorIs(t, err, context.Canceled)
-	require.Nil(t, ch)
+	require.Empty(t, chs)
+
+	// Few channels, timed out context. With tiny 'settleTime' we wait for the context to cancel.
+	duration := 10 * time.Millisecond
+	ctx, cancel = context.WithTimeout(context.Background(), duration)
+	t0 := time.Now()
+	chs, err = ws.Wait(ctx, time.Nanosecond)
+	require.ErrorIs(t, err, context.DeadlineExceeded)
+	require.Empty(t, chs)
+	require.True(t, time.Since(t0) > duration, "expected to wait until context cancels")
+	cancel()
+
+	// One closed channel. Should wait until 'settleTime' expires.
+	close(ch1)
+	t0 = time.Now()
+	chs, err = ws.Wait(context.Background(), duration)
+	require.NoError(t, err)
+	require.ElementsMatch(t, chs, []<-chan struct{}{ch1})
+	require.True(t, time.Since(t0) > duration, "expected to wait until settle time expires")
+
+	// One closed channel, 0 wait time.
+	ws = NewWatchSet()
+	ws.Add(ch2)
+	close(ch2)
+	chs, err = ws.Wait(context.Background(), 0)
+	require.NoError(t, err)
+	require.ElementsMatch(t, chs, []<-chan struct{}{ch2})
 
 	// Many channels
 	for _, numChans := range []int{2, 16, 31, 1024} {